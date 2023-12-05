b3a4eadfb6591303dbe5b48a7b53af3885fa74d7
Crowdbotics <team@crowdbotics.com>
2023-12-05 05:38:58 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "check-cashing1-44865",
+  url: "https://check-cashing1-44865.botics.co",
+  api: "https://check-cashing1-44865.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
